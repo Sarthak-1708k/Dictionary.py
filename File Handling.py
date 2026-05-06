2f9872e6600bfa0b45cc17e719b52b803f1eb8cf
Sarthak-1708k <sarthakkolse999@gmail.com>
2026-05-06 12:00:49 +0530

    Handling.py

---
+++ File Handling.py
@@ -0,0 +1,9 @@
+with open("new_file", "w") as file:
+    data = file.write("Welcome to this file")
+
+with open("new_file", "a") as file:
+    append = file.write("\nThis is new line")
+    
+with open("new_file", "r") as file:
+    data = file.read()
+    print(data)
